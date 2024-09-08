688312ab2a93b2d87d5f995a3066421b602812a1
Saugat <asaugat04@gmail.com>
2024-09-08 22:42:05 +0545

    added controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,35 @@
+import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
-import User from "../models/User";
+import User, { IUser } from "../models/User";
 
 export const register = async (req: Request, res: Response) => {
-  const { userName, email, password } = req.body;
+  const { username, email, password } = req.body;
 
   try {
-    await User.create({ username: userName, email, password });
-    res.status(201).json({ message: "User created successfully" });
-  } catch (error: any) {
-    res.status(400).json({ msg: error.message });
+    // Check if the user already exists
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    // Create a new user instance
+    const newUser: IUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Encrypt the password before saving the user
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(password, salt);
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (err) {
+    console.error("Error during user signup:", err);
+    res.status(500).send("Server error");
   }
 };
 export const login = async (req: Request, res: Response) => {
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,7 +3,8 @@ import { login, register } from "../controllers/authController";
 
 const router = Router();
 
-router.post("/register", register);
+router.post("/signup", register);
 router.post("/login", login);
 
 export default router;
+// app.post("/api/users", async (req: Request, res: Response) => );
