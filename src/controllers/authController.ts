204f77a8aef4ab8502349d8eeb33b1932c97491e
Saugat <asaugat04@gmail.com>
2024-09-14 22:05:37 +0545

    feat:tokens added while signup and login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import User, { IUser } from "../models/User";
 
 const generateHash = async (password: string) => {
@@ -7,6 +9,14 @@ const generateHash = async (password: string) => {
   const pwd = await bcrypt.hash(password, salt);
   return pwd;
 };
+
+const generateToken = (user: IUser) => {
+  return jwt.sign({ id: user._id }, secret, { expiresIn: "1h" });
+};
+
+dotenv.config();
+const secret = process.env.JWT_SECRET || "secret";
+console.log(secret);
 
 export const register = async (req: Request, res: Response) => {
   const { username, email, password } = req.body;
@@ -17,7 +27,6 @@ export const register = async (req: Request, res: Response) => {
     if (user) {
       return res.status(409).json({ msg: "User already exists" });
     }
-
     const newUser: IUser = new User({
       username,
       email,
@@ -26,24 +35,46 @@ export const register = async (req: Request, res: Response) => {
 
     newUser.password = await generateHash(password);
     await newUser.save();
+    const token = generateToken(newUser);
 
-    res.status(201).json({ msg: "User registered successfully" });
+    res.status(201).json({
+      msg: "User registered successfully",
+      token,
+      data: {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+    });
   } catch (err) {
     console.error("Error during user signup:", err);
     res.status(500).send("Server error");
   }
 };
+
 export const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const user: IUser | null = await User.findOne({ email });
     const isMatch = await bcrypt.compare(password, user?.password || "");
+
     if (!isMatch) {
       return res.status(400).json({ msg: "Invalid credentials" });
     }
+
+    if (!user) {
+      return res.status(400).json({ msg: "User does not exist" });
+    }
+
+    const token = generateToken(user);
     res.status(200).json({
       msg: "User logged in successfully",
-      user: { _id: user?._id, username: user?.username, email: user?.email },
+      token,
+      user: {
+        msg: "User logged in successfully",
+        ...user.toJSON(),
+        password: undefined,
+      },
     });
   } catch (error: any) {
     res.status(400).json({ msg: error.message });
